a82bf5017ac3056f13b588534e4ea58943e7df5c
Patrick <treeshole@gmail.com>
2013-08-13 21:36:27 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treeshole::Application.config.secret_key_base = 'e80821118802a5ccfa8a441ad3c0b0f7226b717a9d9a5a1a1c991b209705a4123d7c9691b948fe144ca8d4b2530e826a56f8c087a6a49fbf2f4aae4e2b33b6d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Treeshole::Application.config.session_store :cookie_store, key: '_treeshole_session'
