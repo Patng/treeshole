1fb487ff6daae7597afefe8063d343acd1e65cbb
Patrick <treeshole@gmail.com>
2013-08-31 15:41:41 +0800

    sitemap revised

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,6 +4,7 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://heroku.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
+  add '/about', :changefreq => 'weekly'
+  add '/terms', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
