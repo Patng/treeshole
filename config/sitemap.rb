5210bc02e86998b26b902a4c2ad7c03d137bbb1c
Patrick <treeshole@gmail.com>
2013-08-31 18:18:18 +0800

    bug fixed

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'http://heroku.com'
+SitemapGenerator::Sitemap.default_host = 'http://www.9-cap.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'daily', :priority => 0.9
   add '/about', :changefreq => 'weekly'
